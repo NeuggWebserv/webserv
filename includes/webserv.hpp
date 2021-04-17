8416461fe14aed0e0f3ef599ff64cf2fd8e894e6
sseo <tjdalsemc@gmail.com>
2021-04-17 21:31:42 +0900

    initial webserv header

---
+++ includes/webserv.hpp
@@ -0,0 +1,43 @@
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+
+// CPP Includes
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <limits>
+
+// CPP Containers
+# include <algorithm>
+# include <utility>
+# include <map>
+# include <vector>
+
+// C Includes
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <time.h>
+# include <errno.h>
+# include <dirent.h>
+
+// C system
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+
+// C Network
+# include <sys/socket.h>
+# include <sys/select.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+typedef struct  s_listen
+{
+    unsigned    host;
+    id_t        port;
+}               t_listen;
+
+#endif /* WEBSERV_HPP */
