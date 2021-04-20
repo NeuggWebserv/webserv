cc99a0ee8548f502acbd2b226af47359dd6651ac
junkang <nawaddaing@gmail.com>
2021-04-20 19:53:14 +0900

    Write ConfigUtil.hpp and ConfigUtil.cpp

--- srcs/config/ConfigUtil.hpp
+++ srcs/config/ConfigUtil.hpp
@@ -10,4 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONFIGUTIL_HPP
+#define CONFIGUTIL_HPP
 
+# include "webserv.hpp"
+
+# define parseMap std::map<std::string, void (ConfigServer::*)(fileVector)>
+
+class ConfigServer;
+class ConfigRequest;
+
+typedef struct  s_error_page {
+	std::vector<int>    error_codes; // all of the codes that will be redirected
+	std::string         uri;		// uri to which they are redirected
+}               t_error_page;
+
+bool 			is_digits(const std::string &str);
+unsigned int	str_to_ip(std::string strIp);
+std::string		remove_adjacent_slashes(const std::string &str);
+
+#endif
