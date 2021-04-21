4eb4532fb5e815d250ca7413fcb6c065d66b2156
sseo <tjdalsemc@gmail.com>
2021-04-21 23:46:13 +0900

    update server_socket

---
+++ srcs/server/server_socket.hpp
@@ -0,0 +1,33 @@
+#ifndef SERVER_SOCKET_HPP
+# define SERVER_SOCKET_HPP
+
+# include "config.hpp"
+# include "webserv.hpp"
+
+class Config;
+
+class ServerSocket
+{
+public:
+    ServerSocket(t_listen* lstn);
+    ~ServerSocket();
+
+    void setup();
+    int accept(void);
+    int send(int client_fd);
+    int recv(int client_fd);
+    void do_request(int client_fd, const Config& config);
+    void close_client_fd(int client_fd);
+    int get_fd() const;
+
+private:
+    ServerSocket();
+
+    int                         server_fd;
+    t_listen                    *lstn;
+    struct sockaddr_in          address;
+    std::map<int, std::string>  client_request_mapping;
+    char                        recv_buffer[PACKET_SIZE];
+};
+
+#endif /* SERVER_SOCKET_HPP */
