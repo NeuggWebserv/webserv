d789b5c70f462652a9e93def4b0bd7540e889725
sseo <tjdalsemc@gmail.com>
2021-04-21 23:45:53 +0900

    update server

---
+++ srcs/server/server.hpp
@@ -0,0 +1,35 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include "config.hpp"
+# include "server_socket.hpp"
+# include "webserv.hpp"
+
+class Server
+{
+public:
+    Server();
+
+    ~Server();
+
+    void set_config(const char *filename);
+
+    void set_socket(void);
+
+    void run(void);
+
+private:
+    Config                          config;
+    std::map<int, ServerSocket>     server_socket_mapping;
+    std::map<int, ServerSocket*>    client_socket_mapping;
+    std::vector<int>                ready_to_send;
+    fd_set                          fd_status;
+    size_t                          fd_size;
+    int                             max_fd;
+
+#ifdef TEST
+# define PORT 8080
+#endif
+};
+
+#endif /* SERVER_HPP */
